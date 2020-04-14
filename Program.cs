2a84a78693d45644060b3b91ccc376b5c1e65f4c
szvds <62033452+szvds@users.noreply.github.com>
2020-04-14 17:26:09 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using FluentNHibernate.Conventions.Inspections;
+using Glimpse.AspNet.Tab;
+using Grpc.Core;
+using Humanizer;
+using Microsoft.VisualStudio.Services.Profile;
+using MySqlX.XDevAPI;
+using org.joda.time;
+using Renci.SshNet.Messages;
+using System;
+using System.ServiceModel.Channels;
+using System.Threading.Channels;
+using static Glimpse.AspNet.AlternateType.RouteBase.GetRouteData;
+using static Glimpse.AspNet.AlternateType.RouteBase.GetVirtualPath;
+using static Glimpse.AspNet.AlternateType.RouteConstraint.Match;
+using static Org.BouncyCastle.Bcpg.Attr.ImageAttrib;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+
+const Discord = require('discord.js');
+const Config = require('./config.json');
+
+const Client = new Discord.Client();
+
+Config require(char v)
+{
+    throw new NotImplementedException();
+}
+
+const client = new Discord.Client();
+
+Client.once('ready', () +> {
+    Console.log('Okay boomer');
+    });
+    client.on('message', message => {
+        Console.log(Message.content);
+    });
+    
+    if (Message.content === '!ping') {
+
+    message,Channel.send('pong!.');
+    }
+    else if (Message.content === `${Prefix}Server` {
+        Message.channel.send(`Grpc.Core.Server name: ${message.guild.name}\nTotal members: ${Message.guild.membercount}`);
+    }
+else if (comand === 'kick') {
+internal class comand
+{
+}
+if (!message.mentions.user.size) {
+    return Message.reply(~you need to tage a user to kick them dummy!);
+  }
+const taggeduser = System.ServiceModel.Channels.Message.mentions.users.first();
+
+message.channel.send(`you wanted To kick: ${taggeduser.username}`);
+}
+
+else if (comand === `Avatar`) {
+    if (!message.mentions.users.size) {
+        return Message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ Format: "png", dynamic: true })}>
+}
+  // ...
+  }
+
+else if (command === `avatar`) {
+     if (!message.mentions.users.size) {
+         return Message.channel.send{`Your Avatar: <${message.author.displayAuthorURL({ format: "png", dynamic: true })}>
+    }
+
+    const avatarList = Message.mentions.users.map(User =>
+    {
+        return `${ User.username}`s avatar: <#{user.displayAvatarURL({ format: "png", dynamic: true })}>`;
+    });
+
+    message.channel.send(avatarList);
+}
+
+if (isNaN(amount)) {
+    return Message.reply('that doesent seem to be a valid number')
+} else if (ammount < 2 || amount > 100) {
+    return Message.reply('you need to imput a number between 2 and 100.');
+}
+
+    mesaage.channel.bulkDelete(amount, TruncateExtensions).catch(err => {
+    console.error(err;
+    message.channel.send(`there was an error trying to prune messages in this channel.they might be older than 2 Weeks.');
+});
+
+if (.message.content => ${prefix}rules');
+    {
+const exampleEmbed = new Discod.messageEmbed()
+    .setcolor('#fe00f6')
+    .setTitle('Server guidelines')
+    .setDescipryion('test')
+    .setFooter('test')
+    .message.channel.send
+
+Client.login('Njk5NjE2)Tg3MTU1NzkxODky.XpW_KA.UvsyISGMLJO7HmEx7VTCloZpSCE');
